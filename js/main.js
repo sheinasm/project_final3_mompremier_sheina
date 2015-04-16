cc6696423c140a4694aaafe688c70ed76c8b1b4f
Sheina Mompremier <sheina.sally@gmail.com>
2015-04-16 00:35:32 -0400

    cleanup js

--- js/main.js
+++ js/main.js
@@ -4,31 +4,31 @@ $( document ).ready(function() {
     //JSSOR Slider-------------------------------------------------------->
     if ( $( "slider1_container" ).length ) {
 
-        $( "slider1_container" ).show();
+        $( "slider1_container" ).show( );
 
     }
 
-    var options = {};
-        var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+    var options = { };
+        var jssor_slider1 = new $JssorSlider$( 'slider1_container', options );
 
         //responsive code begin
         //you can remove responsive code if you don't want the slider scales
         //while window resizes
-        function ScaleSlider() {
-            var parentWidth = $('#slider1_container').parent().width();
-            if (parentWidth) {
-                jssor_slider1.$ScaleWidth(parentWidth);
+        function ScaleSlider( ) {
+            var parentWidth = $('#slider1_container').parent( ).width( );
+            if ( parentWidth ) {
+                jssor_slider1.$ScaleWidth( parentWidth );
             }
             else
-                window.setTimeout(ScaleSlider, 30);
+                window.setTimeout( ScaleSlider, 30 );
         }
         //Scale slider after document ready
-        ScaleSlider();
+        ScaleSlider( );
 
         //Scale slider while window load/resize/orientationchange.
-        $(window).bind("load", ScaleSlider);
-        $(window).bind("resize", ScaleSlider);
-        $(window).bind("orientationchange", ScaleSlider);
+        $( window ).bind( "load" , ScaleSlider );
+        $( window ).bind( "resize" , ScaleSlider );
+        $( window ).bind( "orientationchange" , ScaleSlider );
         //responsive code end
 
         //------Trigger-----
@@ -38,12 +38,12 @@ $( document ).ready(function() {
 
     if ( $( ".fancybox" ).length ) {
 
-        $( ".fancybox" ).show();
+        $( ".fancybox" ).show( );
 
     }
 
 
-    $(".fancybox").fancybox();
+    $( ".fancybox" ).fancybox( );
 
 
 
